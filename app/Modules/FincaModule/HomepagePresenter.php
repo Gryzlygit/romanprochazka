e570b0828ef13dee2757b30b270c6bd2a85d68ed
Roman <roman.0.prochazka@seznam.cz>
2021-03-30 21:13:32 +0100

    tokens folder

--- app/Modules/FincaModule/HomepagePresenter.php
+++ app/Modules/FincaModule/HomepagePresenter.php
@@ -6,45 +6,43 @@ use Nette;
 
 class HomepagePresenter extends Nette\Application\UI\Presenter {
 
+    public $savePath;
+
     protected function startup(): void {
         parent::startup();
+        $this->savePath = dirname($_SERVER['SCRIPT_FILENAME']) . '/../files/tokens/';
     }
 
     public function renderSage() {
-        
-    }
-
-    public function renderSwager() {
-        $sales = new \Swagger\Client\Api\SalesQuickEntriesApi();
-        $sales->getSalesQuickEntries();
+        bdump($this->savePath);
     }
 
     public function renderSageApi() {
-        $url = 'https://api.accounting.sage.com/v3.1/sales_invoices?from_date=2021-01-29';
-        $data = ['access_token' => 'eyJhbGciOiJSUzUxMiIsImtpZCI6InE3bnVHWVZRdWtjT0gwQnlEQjZ5UWFhdkx6S2tET0xZQml6TzZoUzVXVUE9In0.eyJqdGkiOiJlMDFmNWVkMC0zMDE5LTQ2OWItYWZlNC1lNWRmZDdhMjRjZmEiLCJpYXQiOjE2MTcwMjc5MTEsImV4cCI6MTYxNzAyODIxMSwiaXNzIjoib2F1dGguYXdzLnNiYy1hY2NvdW50aW5nLnNhZ2UuY29tIiwic3ViIjoiZGQ5YTNkNjgtMjY0Yi00NTY4LWIxMTMtMGU2YzM4MDM2OTMzIiwiYXVkIjoiYXBpLnNiYy1hY2NvdW50aW5nLnNhZ2UuY29tIiwiYXpwIjoiMTNjZTJhNTYtY2JkZi00ZTdiLWIwYmUtMmRlYzgyNGM4MzU1L2Q4M2ZhMTQ1LWNlY2MtNDRiYy1hNzM2LWNiMTUwZTU2MWVlZiIsImNvdW50cnkiOiJFUyIsInNjb3BlcyI6ImFjY291bnRpbmc6cncgY29yZTpydyIsInVzYWdlX3BsYW5fY29kZSI6ImV4dGVybmFsIn0.Pn84pCO8kzk5DJSN4tNfyRqJ3qK0nN_2U3h_hN50ERio3-hlnw_PnLl0lbF1kfC-grrH2gaIQ-TJjgquTMwfv-QbHjjXQkH5uyz55KNIz3xqEOUlIdHX5-a1TWIsDHUIRkps37UTLJQbHhl8SHhEwtq7yTa9INuH7jmmqRc69ZewtTFXTXZQTwtcoOAidciaw-lO_H5AaRNCJwMxrj51VbR9qYIxtH4fcXlmUzrd1H9Ftlo0kSy-bpRy-Cvk1dPb6FB2Fv-PNHPuoww7iqXfl3Pp-9qAFkOq7dxjonHzJTozUobHQFi0yWBHTt9ff5bIDVT7dQUsJsL0EqNvUoSf5rlIboO32lmAFInp2rfo5mg-ZJlivBYKdSQZddlLe852TC6Hg7OwVzeBo7EBiaWTTcji3PyunEHoMs-nXYCTadjGyeptDtedC6YPbbfFRkvM3c_LX8uGJMONl6R7hymo1JM28zTLi-uzlIFNh47RmLVPalz1bQDh4MR4j0ktrt90oAm2ubZfqq002bEcperBWhVJ4Dn-6fQ_PW2tGfSvZR4McfpuMTzECuhsLkzN1x1UGxXl4KhPc6myuNIKhn1MIeB6KHynhAeop7iSSU7vGdb0l179HgyDO51Ovv6EbloWCMP56AQINYQrOMBVdmrUPCI4a9vtjHkpwXkKZ8pqHBw'];
+        $url = 'https://api.accounting.sage.com/v3.1/sales_quick_entries';
+        $accessToken = 'eyJhbGciOiJSUzUxMiIsImtpZCI6InE3bnVHWVZRdWtjT0gwQnlEQjZ5UWFhdkx6S2tET0xZQml6TzZoUzVXVUE9In0.eyJqdGkiOiIzZmRhMTExMy1jOGM3LTQ1YWQtOGUyYS01MTRkYWJiODE4OWIiLCJpYXQiOjE2MTcxMzMyMjAsImV4cCI6MTYxNzEzMzUyMCwiaXNzIjoib2F1dGguYXdzLnNiYy1hY2NvdW50aW5nLnNhZ2UuY29tIiwic3ViIjoiZGQ5YTNkNjgtMjY0Yi00NTY4LWIxMTMtMGU2YzM4MDM2OTMzIiwiYXVkIjoiYXBpLnNiYy1hY2NvdW50aW5nLnNhZ2UuY29tIiwiYXpwIjoiMTNjZTJhNTYtY2JkZi00ZTdiLWIwYmUtMmRlYzgyNGM4MzU1L2Q4M2ZhMTQ1LWNlY2MtNDRiYy1hNzM2LWNiMTUwZTU2MWVlZiIsImNvdW50cnkiOiJFUyIsInNjb3BlcyI6ImFjY291bnRpbmc6cncgY29yZTpydyIsInVzYWdlX3BsYW5fY29kZSI6ImV4dGVybmFsIn0.QldoySy_0ZpTM0iu6QBSYTs7vo5Df4XMWqOwV0bAy-06luChetVSo03l7vpTwx-5_HsqWakjU5X5MfuBS5HznGKUbYhB-KVjrCI-5FPHNWeo_Ot6bnx-DG9RbLV7OL5DZ8Wz1tWNDtHyfLUOBzfLzCntCOgGRxhjftwf49AsasTQqcLv2zInHgQH6Gjdwd4BiDf1MZfQcYy98uxlVhBZkS83IfPmbvF5jh73sOAcTzog_tWIEannKsNb18EI_m8Vw9bNsHyJpEjVUyMGXkcpsqj-xfyAMszfWpNx5LWcVkomsjIGdawa6RsWJGvKc81nH7KKdVOikY5pB_dRFnTPtBRLtJ0U6ipG4AurOFoNqavo0qn_qk1G8Cg5EEEzLfKBf_RSYqcdKcowbgbHkZIGNL5I-PXP9ULtP-jRHvnHTjZE65URwg3KmI8HCVAlGT2wOcqhy52w9_Bslwzv3uFABLb3CKFbS18tWXmG_MBIXS0DODoHOXmRiDodqC_b7GtyVsgI4Xd63XBzb7EDza_Tm6yDrKJCOn9Ly5U5q1kg4PPOCPQk1ALzDpQE2JE-D1y8khbxeG8rgYbiwRbaq8quTQV-l1yBdxLptITBSNUDmiTS8vEpjpDbQRJGAOllHw2U8ZoLtvwNrTFx6U-MEr4MEJ9O03RcnmQqJa6yANWfoNA';
 
         $options = array(
             'http' => array(
-                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                //'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                'header' => "Authorization: Bearer " . $accessToken,
                 'method' => 'GET',
-                'content' => http_build_query($data)
+            //'content' => http_build_query($data)
             )
         );
         $context = stream_context_create($options);
         $resultJson = file_get_contents($url, false, $context);
-        if ($resultJson === FALSE) { /* Handle error */
+        if ($resultJson === FALSE) {
+            
         }
 
+        bdump($resultJson);
         $result = json_decode($resultJson);
         bdump($result);
-        var_dump($result);
+        dump($result);
         die;
-        echo $result->access_token;
     }
 
-    public function renderCallback() {
-        bdump($this->params);
-
+    public function actionCallback() {
         $url = 'https://oauth.accounting.sage.com/token';
         $data = [
             'client_id' => '13ce2a56-cbdf-4e7b-b0be-2dec824c8355/d83fa145-cecc-44bc-a736-cb150e561eef',
@@ -54,7 +52,7 @@ class HomepagePresenter extends Nette\Application\UI\Presenter {
             'redirect_uri' => 'http://localhost/romanprochazka/www/finca.homepage/callback',
         ];
 
-// use key 'http' even if you send the request to https://...
+        // use key 'http' even if you send the request to https://...
         $options = array(
             'http' => array(
                 'header' => "Content-type: application/x-www-form-urlencoded\r\n",
@@ -68,34 +66,12 @@ class HomepagePresenter extends Nette\Application\UI\Presenter {
         }
 
         $result = json_decode($resultJson);
-        //bdump($result);
-        //var_dump($result->access_token);
-
-
-
-
-
-        $url = 'https://api.accounting.sage.com/v3.1/sales_quick_entries';
-        //$data = ['access_token' => $result->access_token];
-
-        $options = array(
-            'http' => array(
-                //'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-                'header' => "Authorization: Bearer ".$result->access_token,
-                'method' => 'GET',
-                //'content' => http_build_query($data)
-            )
-        );
-        $context = stream_context_create($options);
-        $resultJson = file_get_contents($url, false, $context);
-        if ($resultJson === FALSE) {
-        }
-
-        bdump($resultJson);
-        $result = json_decode($resultJson);
         bdump($result);
-        dump($result);
-        die;
+        //var_dump($result->access_token);die;
+        file_put_contents($this->savePath . 'access_token_sage.txt', $result->access_token);
+        file_put_contents($this->savePath . 'refresh_token.txt', $result->refresh_token);
+        $this->flashMessage('tokeny ulozeny');
+        $this->redirect('default');
     }
 
 }
